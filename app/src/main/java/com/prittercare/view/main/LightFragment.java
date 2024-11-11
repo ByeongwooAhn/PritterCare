2e6ce40c4da6a9a365a2ba0f9ef709677ab526c1
pms9158@wku.ac.kr <pms9158@wku.ac.kr>
2024-11-11 17:02:23 +0900

    계정 찾기 레이아웃 수정 회원가입 레이아웃 구현중

--- app/src/main/java/com/prittercare/view/main/LightFragment.java
+++ app/src/main/java/com/prittercare/view/main/LightFragment.java
@@ -61,6 +61,6 @@ public class LightFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_light, container, false);
+        return inflater.inflate(R.layout.fragment_main_light, container, false);
     }
 }
--- app/src/main/java/com/prittercare/view/main/TemperatureAndHumidtyFragment.java
+++ app/src/main/java/com/prittercare/view/main/TemperatureAndHumidtyFragment.java
@@ -61,6 +61,6 @@ public class TemperatureAndHumidtyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control_temperature_and_humidty, container, false);
+        return inflater.inflate(R.layout.fragment_main_temperature_and_humidty, container, false);
     }
 }
