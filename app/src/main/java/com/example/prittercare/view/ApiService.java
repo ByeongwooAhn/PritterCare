f72bc0ec852814dd08ab6d1025ed444b7a8415ff
DESKTOP-DLKDAER\am608 <abyungwoo@gmail.com>
2024-11-13 22:22:35 +0900

    feat: 로그인 기능 구현
    1. 로그인 화면의 텍스트박스에 입력된 아이디와 비밀번호 값을 서버로 전송
    2. 어플에서 서버로 전송한 아이디와 비밀번호 값을 받아서 데이터베이스에 SELECT WHERE 문으로 조회
    3. 조회 결과에 따라 true와 false를 반환하여 어플로 전송
    4. 서버에서 전송받은 tre, false 값에 따라 로그인의 성공 유무를 나타내는 메시지 출려과 함께 로그인 or 로그인 실패

--- app/src/main/java/com/example/prittercare/view/ApiService.java
+++ app/src/main/java/com/example/prittercare/view/ApiService.java
@@ -1,5 +1,7 @@
 package com.example.prittercare.view;
 
+import com.example.prittercare.view.main.LoginRequest;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
@@ -7,4 +9,7 @@ import retrofit2.http.POST;
 public interface ApiService {
     @POST("signup")
     Call<Void> signUp(@Body SignUpRequest signUpRequest);
+
+    @POST("login")
+    Call<Boolean> logIn(@Body LoginRequest loginRequest);
 }
