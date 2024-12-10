d14e99fc029f36b74b805853eeb0896eaf50f536
pms9158 <pms9158@wku.ac.kr>
2024-12-10 19:14:53 +0900

    feat. QR코드 스캔 기능 구현 - QR코드 스캔을 통해서 시리얼 넘버 추출 가능.
    Todo
    - 시리얼 넘버를 가지고 케이지 등록 구현 작업
    - DB연동 작업 (CRUD)

--- app/src/main/java/com/example/prittercare/view/fragments/LightFragment.java
+++ app/src/main/java/com/example/prittercare/view/fragments/LightFragment.java
@@ -3,6 +3,7 @@ package com.example.prittercare.view.fragments;
 import android.content.Context;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
